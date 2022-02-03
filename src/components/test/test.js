b42f8f4f628c0ff18e0499e4a4c8d9edbe7247e4
maxhasl <maxhasl@ukr.net>
2022-02-03 16:10:59 +0200

    change test img class

--- src/components/test/test.js
+++ src/components/test/test.js
@@ -10,7 +10,7 @@ const Test = () => {
 
   const download = () => {
     console.log('Download');
-    const html = `<img src="${NAVI}"><div class="${cl.test}">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div><div class="${cl.test2}">Text here!!!</div><div class="${cl.test2}">Text here!!!</div><div class="${cl.test}">Text here!!!</div>`;
+    const html = `<img class="${cl.testImg}" src="${NAVI}"><div class="${cl.test}">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div><div class="${cl.test2}">Text here!!!</div><div class="${cl.test2}">Text here!!!</div><div class="${cl.test}">Text here!!!</div>`;
 
     doc.html(html, {
       callback: function (doc) {
@@ -38,7 +38,7 @@ const Test = () => {
         name="img"
         accept="image/*"
       ></input>
-      <img alt="logo" src={image} />
+      <img className={cl.testImg} alt="logo" src={image} />
     </div>
   );
 };
