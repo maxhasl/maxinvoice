6f7d3d9ed7bbfde298003483c74c415505b1bc19
maxhasl <maxhasl@ukr.net>
2022-02-08 17:56:20 +0200

    add Invoice

--- src/components/invoice/invoice.js
+++ src/components/invoice/invoice.js
@@ -1,10 +1,13 @@
+import InputFile from '../input-file';
 import styles from './invoice.module.scss';
 
 const Invoice = () => {
   return (
     <section className={styles.body}>
       <div className={styles.container}>
-        <form></form>
+        <form>
+          <InputFile />
+        </form>
       </div>
     </section>
   );
