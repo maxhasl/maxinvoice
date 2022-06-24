553bb4c5d69a11fa141633df082fea584598e900
maxhasl <maxhasl@ukr.net>
2022-06-24 14:36:06 +0300

    makePdf middleware logic

--- src/redux/middleware/make-pdf/make-pdf.js
+++ src/redux/middleware/make-pdf/make-pdf.js
@@ -6,38 +6,36 @@ const middleware = (store) => (next) => async (action) => {
 
   const { payload, ...rest } = action;
 
-  try {
-    const doc = new jsPDF().setFont('Ubuntu');
+  const doc = new jsPDF().setFont('Ubuntu');
 
-    doc.html(template(store.getState()), {
-      callback: (doc) => {
-        doc.save('a4.pdf');
-        next({
-          ...rest,
-          payload: {
-            ...rest.payload,
-            done: true,
-            error: false,
-            process: false,
-          },
-        });
-      },
-      margin: [5, 10, 5, 10],
-      width: 190,
-      windowWidth: 800,
-      autoPaging: 'text',
-    });
-  } catch (error) {
-    next({
-      ...rest,
-      payload: {
-        ...rest.payload,
-        done: false,
-        error: true,
-        process: false,
-      },
-    });
-  }
+  doc.html(template(store.getState()), {
+    callback: (doc) => {
+      doc.save('a4', { returnPromise: true }).then(
+        () =>
+          next({
+            ...rest,
+            payload: {
+              ...rest.payload,
+              done: true,
+              error: false,
+            },
+          }),
+        () =>
+          next({
+            ...rest,
+            payload: {
+              ...rest.payload,
+              done: false,
+              error: true,
+            },
+          })
+      );
+    },
+    margin: [5, 10, 5, 10],
+    width: 190,
+    windowWidth: 800,
+    autoPaging: 'text',
+  });
 };
 
 export default middleware;
